29ef5600c052588e25f11f32d917311fccbd1570
syberflea <floid80@inbox.ru>
2017-08-02 22:08:37 +0300

    First bot deploying

---
+++ my_bot.py
@@ -0,0 +1,40 @@
+import requests
+
+#330318626:AAFtv8sB6cs8PQ5Tp88pB0_4Ea46aE6ahBk
+
+url = "https://api.telegram.org/bot330318626:AAFtv8sB6cs8PQ5Tp88pB0_4Ea46aE6ahBk/"
+
+
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+
+def last_update(data):
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+#
+send_mess(chat_id, 'Your message goes here')
+
+def main():
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+    sleep(1)
+
+if __name__ == '__main__':
+    main()
