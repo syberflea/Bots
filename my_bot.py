b0dcd3353670fbc9058c69659e76d4bc06949566
syberflea <floid80@inbox.ru>
2017-08-24 23:26:30 +0300

    'Codementor'

--- my_bot.py
+++ my_bot.py
@@ -1,38 +1,69 @@
+import json
 import requests
+import time
+import urllib
 
-#330318626:AAFtv8sB6cs8PQ5Tp88pB0_4Ea46aE6ahBk
-
-url = "https://api.telegram.org/bot330318626:AAFtv8sB6cs8PQ5Tp88pB0_4Ea46aE6ahBk/"
-
-
-def get_updates_json(request):
-    params = {'timeout': 100, 'offset': None}
-    response = requests.get(request + 'getUpdates', data=params)
-    return response.json()
+TOKEN = "330318626:AAFtv8sB6cs8PQ5Tp88pB0_4Ea46aE6ahBk"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
-def last_update(data):
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
 
-def get_chat_id(update):
-    chat_id = update['message']['chat']['id']
-    return chat_id
 
-def send_mess(chat, text):
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates(offset=None):
+    url = URL + "getUpdates"
+    if offset:
+        url += "?offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def echo_all(updates):
+    for update in updates["result"]:
+        text = update["message"]["text"]
+        chat = update["message"]["chat"]["id"]
+        send_message(text, chat)
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+
+def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
 
 
 def main():
-    update_id = last_update(get_updates_json(url))['update_id']
+    last_update_id = None
     while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-           update_id += 1
-    sleep(1)
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
 
 if __name__ == '__main__':
     main()
