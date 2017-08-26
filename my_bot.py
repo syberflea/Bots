d98a2e7403e188bb6b1c04d7daf6c5d4e624fc4d
syberflea <floid80@inbox.ru>
2017-08-26 22:45:30 +0300

    second one;

--- my_bot.py
+++ my_bot.py
@@ -62,7 +62,7 @@ def main():
         if len(updates["result"]) > 0:
             last_update_id = get_last_update_id(updates) + 1
             echo_all(updates)
-        time.sleep(0.5)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
